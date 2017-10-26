5749e549315c3487f1004f1e7fbbf40c39c4f87a
HenriWessels <33045808+HenriWessels@users.noreply.github.com>
2017-10-25 19:34:03 -0600

    update iterate.py
    added the two average options

--- iterate.py
+++ iterate.py
@@ -1,25 +1,49 @@
-
 names = ["Lewis", "Ronald", "Bingstrom", "Badjen"]
-places = ["1st", "3rd", "2nd", "4th"]
+times = [7.2, 9.9,14.2, 8.9]
 
 
 def iterate(list):
 	for item in list:
 		print item
 		
-def print_place(names, places):
+def print_time(names, times):
 	for i in range (0, len(names)):
-		print names[i] , "finished" , places[i]
+		print names[i] , "finished" , times[i]
 		
-def congrats (names, places):
+def congrats (names, times):
 	for i in range (0, len(names)):
-		if places[i] == "1st"
+		if times[i] <= 8
 			print "You're okay"
-		elif places[i] == "2nd"
-			print "second place is first loser"
-		else places[i] == "3rd"
-			print "git gud"
-			
 
-print_place(names, places)
-congrats(names, places)
+def average (times):
+	total = 0
+	for n in times (0, len(times)):
+		total += n
+		
+	return total / len(times)
+	
+def average_w_o_Bottom_two_times(times):
+	current_min = times [0]
+	current_min_placement = -1
+	for n in times:
+		if n <= current_min = n
+		
+	scores.remove(current_min)
+	current_min2 = times = times[0]
+	
+	for n in times:
+		if n <= current_min2:
+			current_min2 = n
+			
+	scores.remove(current_min2)
+	
+	sum_w_o_Bottom_two_times = 0
+	for n in times:
+		sum_w_o_Bottom_two_times += n
+		
+	return sum_w_o_Bottom_two_times / len(scores)
+
+		
+
+print_place(names, times)
+congrats(names, times)
